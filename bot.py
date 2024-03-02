ebba0a1e92e873b276b72e3c367eb4334ff5735b
Alex <112178448+Alx424@users.noreply.github.com>
2024-03-02 18:48:52 +0000

    finished bot (for now)

--- bot.py
+++ bot.py
@@ -1,16 +1,29 @@
 import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}'.format(self.user))
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!')
+        if message.content.startswith('$meme'):
+            await message.channel.send(get_meme())
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
 client.run(os.environ["BOT_TOKEN"])
 
-async def on_message(self, message):
-    if message.author == self.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello World!')
