7bef24641955e23903fceb7a0a5a9d56814df5f9
Alex <112178448+Alx424@users.noreply.github.com>
2024-03-02 22:54:08 +0000

    added NASA API functionality for the 'astronomy picture of the day'

--- bot.py
+++ bot.py
@@ -14,6 +14,10 @@ def get_boredActivity():
   response = requests.get('https://www.boredapi.com/api/activity')
   json_data = json.loads(response.text)
   return json_data['activity']
+def get_AstronomyPicOfTheDay():
+    response = requests.get('https://api.nasa.gov/planetary/apod?api_key=CovdJv0F7rn2uFCfcwEv7WBwqOOg5GZBsvUDxCCz')
+    json_data = json.loads(response.text)
+    return json_data['title'] + json_data['hdurl'] + '\n' + json_data['explanation']
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -22,13 +26,15 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
         if message.content.startswith('$hello'):
-            await message.channel.send('Hello there! How can I assist you? Type $commands to see a list of commands.')
-        if message.content.startswith('$commands'):
-            await message.channel.send('$hello - greet the bot \n$commands - a list of commands \n$meme - show a random meme \n$bored - a random activity to do when you\'re bored')
+            await message.channel.send('Hello there! How can I assist you? Type $help to see a list of commands.')
+        if message.content.startswith('$help'):
+            await message.channel.send('$hello - greet the bot \n$help - a list of commands \n$meme - show a random meme \n$bored - a random activity to do when you\'re bored \n$apod - shows the astronomical picture of the day from NASA')
         if message.content.startswith('$meme'):
             await message.channel.send(get_meme())
         if message.content.startswith('$bored'):
             await message.channel.send(get_boredActivity())
+        if message.content.startswith('$apod'):
+            await message.channel.send(get_AstronomyPicOfTheDay)
 intents = discord.Intents.default()
 intents.message_content = True
 
