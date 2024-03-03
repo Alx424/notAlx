d68364e46a324b8b9221fc9eb95398e42668b82f
Alex <112178448+Alx424@users.noreply.github.com>
2024-03-03 15:25:04 +0000

    added dark humour

--- bot.py
+++ bot.py
@@ -18,8 +18,8 @@ def get_AstronomyPicOfTheDay():
     response = requests.get('https://api.nasa.gov/planetary/apod?api_key=CovdJv0F7rn2uFCfcwEv7WBwqOOg5GZBsvUDxCCz')
     json_data = json.loads(response.text)
     return '# Astronomical Picture Of the Day\n## ' + json_data['title'] + '\n' + json_data['url'] + '\n' + json_data['explanation'] + ''
-def get_insult():
-    response = requests.get('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+def get_darkhumour():
+    response = requests.get('https://v2.jokeapi.dev/joke/Dark?blacklistFlags=nsfw,religious,political,racist,sexist,explicit')
     json_data = json.loads(response.text)
     return json_data['insult']
 
@@ -32,13 +32,15 @@ class MyClient(discord.Client):
         if message.content.startswith('$hello'):
             await message.channel.send('Hello there! How can I assist you? Type $help to see a list of commands.')
         if message.content.startswith('$help'):
-            await message.channel.send('$hello - greet the bot \n$help - a list of commands \n$meme - show a random meme \n$bored - a random activity to do when you\'re bored \n$apod - shows the astronomical picture of the day from NASA')
+            await message.channel.send('$hello - greet the bot \n$help - a list of commands \n$meme - show a random meme \n$bored - a random activity to do when you\'re bored \n$apod - shows the astronomical picture of the day from NASA\n$darkhumour - gives a random dark joke')
         if message.content.startswith('$meme'):
             await message.channel.send(get_meme())
         if message.content.startswith('$bored'):
             await message.channel.send(get_boredActivity())
         if message.content.startswith('$apod'):
             await message.channel.send(get_AstronomyPicOfTheDay())
+        if message.content.startswith('$darkhumour'):
+            await message.channel.send(get_darkhumour)
 intents = discord.Intents.default()
 intents.message_content = True
 
